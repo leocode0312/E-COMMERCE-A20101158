d8b1f14828cc0a2bdee5df29f399b666597e80bf
leonardo <leocardenasllerenal@gmail.com>
2025-11-17 23:16:59 -0500

    creacion del modelo User

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true, trim: true, minlegth: 2 },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true},
+    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+}, { timestamps: true});
+
+//hash para la contraseña antes de guardar
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const rounds = parseInt(process.env.SALT_ROUNDS || '10', 10);
+    const salt = await bcrypt.genSalt(rounds);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.comparePassword = function(candidate) {
+    return bcrypt.compare(candidate, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
